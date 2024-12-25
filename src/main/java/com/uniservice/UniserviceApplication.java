346f065c09f915656e73c4e50923e97750040463
Aryan Kumar Singh <aryankrsingh7@gmail.com>
2024-12-25 23:55:59 +0530

    test

---
+++ src/main/java/com/uniservice/UniserviceApplication.java
@@ -0,0 +1,14 @@
+package com.uniservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UniserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniservice/UniserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
